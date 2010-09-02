46f40529a0a3a42730dbf01dc64fb2c755bab9a3
nsaeki <nobuo.saeki@gmail.com>
2010-09-02 12:26:52 +0900

    Added msmlc to smart-compile.

--- rc.d/50smart-compile.el
+++ rc.d/50smart-compile.el
@@ -2,6 +2,7 @@
 (global-set-key "\C-cc" 'smart-compile)
 (setq smart-compile-alist
       (append '(("\\.pl\\'" . "perl -w %f")
-		("\\.rb\\'" . "ruby -w %f"))
+		("\\.rb\\'" . "ruby -w %f")
+		("\\.mxml\\'" . "mxmlc %f")
+		("\\.as\\'" . "mxmlc %f"))
 	      smart-compile-alist))
-;;; TODO customize to fail back into executable-interpret
