c56886d19783224d78061d30f7f2d3066620991f
Nobuo Saeki <nobuo.saeki@gmail.com>
2010-09-04 18:21:26 +0900

    Fixed function in auto-complete-config.el to work with yasnippet 0.6.1c.

--- rc.d/50auto-complete.el
+++ rc.d/50auto-complete.el
@@ -5,5 +5,22 @@
   (define-key ac-mode-map (kbd "M-TAB") 'auto-complete)
   (ac-config-default))
 
+;; fixed auto-complete-config.el to work with yasnippet 0.6.1c
+(defun ac-yasnippet-candidate-1 (table)
+  (with-no-warnings
+    (let ((hashtab (ac-yasnippet-table-hash table))
+          (parent (yas/snippet-table-parents table))
+          candidates)
+      (maphash (lambda (key value)
+                 (push key candidates))
+               hashtab)
+      (setq candidates (all-completions ac-prefix (nreverse candidates)))
+      (when parent
+        (mapc (lambda (table)
+                (setq candidates
+                      (append candidates (ac-yasnippet-candidate-1 table))))
+              parent))
+      candidates)))
+
 (global-set-key "\M-/" 'auto-complete)
 (setq ac-use-menu-map t)
