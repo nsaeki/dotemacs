ad70b5bf9374843b74a4db7026c064db0d028a1e
Nobuo Saeki <nobuo.saeki@gmail.com>
2010-09-04 16:56:20 +0900

    added camelCase-mode and isearch-yank-symbol

--- rc.d/30utils.el
+++ rc.d/30utils.el
@@ -22,6 +22,29 @@
 ;(require 'develock)
 ;(require 'jaspace)
 
+;; camelCase-mode
+;; http://www.eecs.ucf.edu/~leavens/emacs/camelCase/camelCase-mode.el
+(require 'camelCase)
+
+;; ThingAtPoint and isearch-word-at-point
+(require 'thingatpt)
+(defun isearch-yank-symbol ()
+  "*Put symbol at current point into search string."
+  (interactive)
+  (let ((sym (symbol-at-point)))
+    (if sym
+        (progn
+          (setq isearch-regexp t
+                isearch-string (concat "\\_<"
+                                       (regexp-quote (symbol-name sym)) "\\_>")
+                isearch-message (mapconcat
+                                 'isearch-text-char-description isearch-string "")
+                isearch-yank-flag t))
+      (ding)))
+  (isearch-search-and-update))
+(define-key isearch-mode-map "\M-w" 'isearch-yank-symbol)
+
+
 ;; (auto-install-batch "sequential-command")
 (require 'sequential-command-config)
 (sequential-command-setup-keys)
