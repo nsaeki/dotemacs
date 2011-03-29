7fb93f8f1a34c53c184e641350590deca59dfb15
nsaeki <nobuo.saeki@gmail.com>
2011-03-29 14:30:22 +0900

    Changed run level of elpa.

--- rc.d/60actionscript.el
+++ rc.d/60actionscript.el
@@ -1,5 +1,7 @@
 ;; (require 'actionscript-mode)
-(autoload 'actionscript-mode "actionscript-mode" "Major mode for actionscript." t)
+;;(autoload 'actionscript-mode "actionscript-mode" "Major mode for actionscript." t)
+(autoload 'actionscript-mode "actionscript-mode-haas-7.0" "Major mode for actionscript." t)
+
 (add-to-list 'auto-mode-alist '("\\.as$" . actionscript-mode))
 
 ;; mxml
