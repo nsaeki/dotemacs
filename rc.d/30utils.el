d72047abf592323412c1df8dbb21ee1f93b6504d
nsaeki <nobuo.saeki@gmail.com>
2011-04-01 21:26:56 +0900

    added autopair.

--- rc.d/30utils.el
+++ rc.d/30utils.el
@@ -89,3 +89,7 @@
 (define-key global-map (kbd "C-M-z") 'point-redo)
 ;; (define-key global-map [f5] 'point-undo)
 ;; (define-key global-map [f6] 'point-redo)
+
+;; AutoPairs
+(require 'autopair)
+(autopair-global-mode)
