e74f5d23704593740b5684f091d2a448b035eadb
nsaeki <nobuo.saeki@gmail.com>
2011-04-01 21:33:55 +0900

    c

--- rc.d/50clojure.el
+++
@@ -1,7 +0,0 @@
-(require 'slime)
-(setq slime-net-coding-system 'utf-8-unix
-      slime-protocol-version 'ignore)
-(slime-setup '(slime-repl))
-;(slime-setup '(slime-repl slime-fancy slime-banner))
-
-(add-to-list 'auto-mode-alist '("\\.clj$" . lisp-mode))
