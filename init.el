bee36e3d0992004772099e879ff47fc16f60eb7b
Nobuo Saeki <nobuo.saeki@gmail.com>
2011-04-03 16:09:28 +0900

    c

--- init.el
+++ init.el
@@ -99,7 +99,7 @@
   (let (path)
     (dolist (path paths paths)
       (let ((default-directory
-	      (expand-file-name (concat user-emacs-directory path))))
+	      (expand-file-name (concat "~/.emacs.d/" path))))
 	(add-to-list 'load-path default-directory)
 	(if (fboundp 'normal-top-level-add-subdirs-to-load-path)
 	    (normal-top-level-add-subdirs-to-load-path))))))
