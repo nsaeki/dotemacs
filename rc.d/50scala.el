9b91557d5189ce30dd752a484c382daa3c49cce5
Nobuo Saeki <nobuo.saeki@gmail.com>
2011-06-20 23:17:06 +0900

    moved ensime from elisp to plugins directory.

--- rc.d/50scala.el
+++ rc.d/50scala.el
@@ -1,6 +1,6 @@
 (add-to-list 'load-path "~/.emacs.d/elisp/scala-mode/")
 (require 'scala-mode-auto)
 
-(add-to-list 'load-path "~/.emacs.d/elisp/ensime_2.8.1-0.4.4/elisp/")
+(add-to-list 'load-path "~/.emacs.d/plugins/ensime/elisp/")
 (require 'ensime)
 (add-hook 'scala-mode-hook 'ensime-scala-mode-hook)
