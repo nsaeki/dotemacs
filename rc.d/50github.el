d3b2483edc6b90d8161661eca1cef622b231d2d7
Nobuo Saeki <nobuo.saeki@gmail.com>
2012-02-24 01:22:34 +0900

    added gist.el

---
+++ rc.d/50github.el
@@ -0,0 +1,10 @@
+;; (install-elisp "https://raw.github.com/defunkt/gist.el/master/gist.el")
+(require 'gist)
+
+(setq github-user "nsaeki")
+(setq github-token "c5a397b7bfe9e3a6d011768737d3057f")
+
+;; (install-elisp-from-gist "467982")
+;; or 
+;; (install-elisp "https://raw.github.com/gist/467982/a61f47a94f43d24fd10d89e35d49ed41e36a2038/anything-gist.el")
+(require 'anything-gist)
