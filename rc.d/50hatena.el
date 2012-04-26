835e62c32fbb377d52870aa26a628e4c63e2117f
Nobuo Saeki <nobuo.saeki@gmail.com>
2012-04-27 01:32:45 +0900

    added hatena-diary-mode

---
+++ rc.d/50hatena.el
@@ -0,0 +1,4 @@
+(add-to-list 'load-path "~/.emacs.d/elisp/hatena-diary")
+(require 'hatena-diary-mode)
+(setq hatena-usrid "ikeas")
+(setq hatena-twitter-flag nil)
--- rc.d/XXhatena.el
+++
@@ -1,3 +0,0 @@
-(require 'simple-hatena-mode)
-(setq simple-hatena-default-id "ikeas")
-(setq simple-hatena-bin "~/.emacs.d/bin/hw.pl")
