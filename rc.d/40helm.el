7d0e897f493b3e67e287863182b05c1b32333c11
Nobuo Saeki <saeki@booklista.co.jp>
2012-11-03 16:01:48 +0900

    added untracked files.

---
+++ rc.d/40helm.el
@@ -0,0 +1,45 @@
+(require 'helm-config)
+(require 'helm-match-plugin)
+;; (require 'heml-show-completion)
+(require 'helm-grep nil t)
+
+(setq helm-for-files-preferred-list
+      '(helm-c-source-ffap-line
+	helm-c-source-ffap-guesser
+	helm-c-source-buffers-list
+	helm-c-source-recentf
+	helm-c-source-bookmarks
+	helm-c-source-file-cache
+	helm-c-source-files-in-current-dir
+        ))
+
+;; (cond ((eq system-type 'darwin)
+;;        (add-to-list 'helm-for-files-prefered-list
+;;                     helm-c-source-mac-spotlight))
+;;       ((eq system-type 'gnu/linux)
+;;        (add-to-list 'helm-for-files-prefered-list
+;;                     helm-c-source-locate)))
+
+(global-set-key (kbd "C-;") 'helm-for-files)
+(global-set-key (kbd "C-'") 'helm-M-x)
+(global-set-key (kbd "C-.") 'helm-imenu)
+(global-set-key (kbd "M-y") 'helm-show-kill-ring)
+
+;; (require 'helm-etags)
+;; (define-key global-map (kbd "M-.") 'helm-etags-select-from-here)
+
+;; (install-elisp-from-emacswiki "descbinds-helm.el")
+(require 'helm-descbinds)
+(helm-descbinds-install)
+
+(require 'helm-c-moccur)
+(setq moccur-split-word t)
+(global-set-key (kbd "M-o") 'helm-c-moccur-occur-by-moccur)
+(global-set-key (kbd "C-M-o") 'helm-c-moccur-dmoccur)
+(add-hook 'dired-mode-hook
+          '(lambda ()
+             (local-set-key (kbd "O") 'helm-c-moccur-dired-do-moccur-by-moccur)))
+(define-key isearch-mode-map (kbd "M-o") 'helm-c-moccur-from-isearch)
+
+(require 'helm-migemo)
+;;(setq helm-use-migemo t)
---
+++ rc.d/70js.el
@@ -0,0 +1,106 @@
+(autoload 'js2-mode "js2" nil t)
+;(autoload 'js-mode "js" nil t)
+;(defalias 'javascript-mode 'js2-mode)
+(add-to-list 'auto-mode-alist '("\\.js$" . js2-mode))
+
+;; http://8-p.info/emacs-javascript.html
+(defun indent-and-back-to-indentation ()
+  (interactive)
+  (indent-for-tab-command)
+  (let ((point-of-indentation
+         (save-excursion
+           (back-to-indentation)
+           (point))))
+    (skip-chars-forward "\s " point-of-indentation)))
+;;(define-key js2-mode-map "\C-i" 'indent-and-back-to-indentation)
+(setq js2-cleanup-whitespace nil
+      js2-mirror-mode nil
+      js2-bounce-indent-flag nil)
+
+;; http://mihai.bazon.net/projects/editing-javascript-with-emacs-js2-mode
+;; http://d.hatena.ne.jp/speg03/20091011/1255244329
+
+;; http://16777215.blogspot.com/2011/05/emacs23-js2-mode-without-espresso.html
+;(autoload 'espresso-mode "espresso")
+
+(defun my-js2-indent-function ()
+  (interactive)
+  (save-restriction
+    (widen)
+    (let* ((inhibit-point-motion-hooks t)
+           (parse-status (save-excursion (syntax-ppss (point-at-bol))))
+           (offset (- (current-column) (current-indentation)))
+           (indentation (espresso--proper-indentation parse-status))
+           node)
+
+      (save-excursion
+
+        ;; I like to indent case and labels to half of the tab width
+        (back-to-indentation)
+        (if (looking-at "case\\s-")
+            (setq indentation (+ indentation (/ espresso-indent-level 2))))
+
+        ;; consecutive declarations in a var statement are nice if
+        ;; properly aligned, i.e:
+        ;;
+        ;; var foo = "bar",
+        ;;     bar = "foo";
+        (setq node (js2-node-at-point))
+        (when (and node
+                   (= js2-NAME (js2-node-type node))
+                   (= js2-VAR (js2-node-type (js2-node-parent node))))
+          (setq indentation (+ 4 indentation))))
+
+      (indent-line-to indentation)
+      (when (> offset 0) (forward-char offset)))))
+
+(defun my-indent-sexp ()
+  (interactive)
+  (save-restriction
+    (save-excursion
+      (widen)
+      (let* ((inhibit-point-motion-hooks t)
+             (parse-status (syntax-ppss (point)))
+             (beg (nth 1 parse-status))
+             (end-marker (make-marker))
+             (end (progn (goto-char beg) (forward-list) (point)))
+             (ovl (make-overlay beg end)))
+        (set-marker end-marker end)
+        (overlay-put ovl 'face 'highlight)
+        (goto-char beg)
+        (while (< (point) (marker-position end-marker))
+          ;; don't reindent blank lines so we don't set the "buffer
+          ;; modified" property for nothing
+          (beginning-of-line)
+          (unless (looking-at "\\s-*$")
+            (indent-according-to-mode))
+          (forward-line))
+        (run-with-timer 0.5 nil '(lambda(ovl)
+                                   (delete-overlay ovl)) ovl)))))
+
+(defun my-js2-mode-hook ()
+  (require 'espresso)
+  (setq espresso-indent-level 4
+        indent-tabs-mode nil
+        c-basic-offset 4)
+  (c-toggle-auto-state 0)
+  (c-toggle-hungry-state 1)
+  (set (make-local-variable 'indent-line-function) 'my-js2-indent-function)
+  ;;(define-key js2-mode-map [(meta control |)] 'cperl-lineup)
+  (define-key js2-mode-map "\C-\M-\\"
+    '(lambda()
+       (interactive)
+       (insert "/* -----[ ")
+       (save-excursion
+         (insert " ]----- */"))
+       ))
+  (define-key js2-mode-map "\C-m" 'newline-and-indent)
+  ;;(define-key js2-mode-map [(backspace)] 'c-electric-backspace)
+  ;;(define-key js2-mode-map [(control d)] 'c-electric-delete-forward)
+  (define-key js2-mode-map "\C-\M-q" 'my-indent-sexp)
+  (define-key js2-mode-map "\C-i" 'indent-and-back-to-indentation)
+  (if (featurep 'js2-highlight-vars)
+    (js2-highlight-vars-mode))
+  (message "My JS2 hook"))
+
+(add-hook 'js2-mode-hook 'my-js2-mode-hook)
