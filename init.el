f2efe5d18c10d6bb5802d693339e74ad055e159e
Nobuo Saeki <nobuo.saeki@gmail.com>
2012-11-04 00:13:31 +0900

    changed scroll setting.

--- init.el
+++ init.el
@@ -17,6 +17,12 @@
 (defalias 'yes-or-no-p 'y-or-n-p)
 (setq read-file-name-completion-ignore-case t)
 
+;; scroll one line at a time (less "jumpy" than defaults)
+(setq mouse-wheel-scroll-amount '(1 ((shift) . 1))) ;; one line at a time
+(setq mouse-wheel-progressive-speed nil) ;; don't accelerate scrolling
+(setq mouse-wheel-follow-mouse 't) ;; scroll window under mouse
+(setq scroll-step 1) ;; keyboard scroll one line at a time
+
 ;; keybindings
 ;(global-set-key "\C-cc" 'compile)
 (global-set-key [M-return] 'indent-new-comment-line)
