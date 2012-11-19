67bfea383355e93fbe4b905d290aa608b4af00f5
Nobuo Saeki <nobuo.saeki@gmail.com>
2012-11-20 01:40:29 +0900

    removed egg.

--- rc.d/70git.el
+++ rc.d/70git.el
@@ -1,14 +1,8 @@
 (require 'magit)
 (require 'key-chord)
-;; (key-chord-define-global "vc" 'magit-status)
-
-(require 'egg)
-(autoload 'egg-grep "egg-grep" nil t)
-;; (setq egg-auto-update t)
-;; (key-chord-define-global "vc" 'egg-status)
+;; (key-chord-define-global "vc" 'magit-status) ;; moved to 40key-chord.el
 
 (require 'helm-ls-git)
 (when (require 'helm-files)
   (add-to-list 'helm-for-files-preferred-list helm-c-source-ls-git-status t)
-  (add-to-list 'helm-for-files-preferred-list helm-c-source-ls-git t)
-  )
+  (add-to-list 'helm-for-files-preferred-list helm-c-source-ls-git t))
