e74f6df163b195d3a5b4a319c63ee84e830ec67a
saeki <saeki@booklista.co.jp>
2013-07-09 18:44:16 +0900

    Changed header background color in ReST mode.

---
+++ rc.d/70rest.el
@@ -0,0 +1,20 @@
+;; http://d.hatena.ne.jp/LaclefYoshi/20100922/1285125722
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(rst-level-1-face ((t (:foreground "LightSkyBlue"))) t)
+ '(rst-level-2-face ((t (:foreground "LightGoldenrod"))) t)
+ '(rst-level-3-face ((t (:foreground "Cyan1"))) t)
+ '(rst-level-4-face ((t (:foreground "chocolate1"))) t)
+ '(rst-level-5-face ((t (:foreground "PaleGreen"))) t)
+ '(rst-level-6-face ((t (:foreground "Aquamarine"))) t))
+ ;; (rst-level-7-face ((t (:foreground "LightSteelBlue"))) t)  ;; メモ
+ ;; (rst-level-8-face ((t (:foreground "LightSalmon"))) t)
+(custom-set-variables
+  ;; custom-set-variables was added by Custom.
+  ;; If you edit it by hand, you could mess it up, so be careful.
+  ;; Your init file should contain only one such instance.
+  ;; If there is more than one, they won't work right.
+ '(rst-level-face-base-light 50))
