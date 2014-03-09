394f78c13c75ce799fd11e804336e06956e3adda
Nobuo Saeki <nobuo.saeki@gmail.com>
2014-03-09 12:29:20 +0900

    removed ruby-electric and auto-pair. added electric-pair-mode.

--- rc.d/30utils.el
+++ rc.d/30utils.el
@@ -1,29 +1,9 @@
-;(require 'memo-mode)
-;(require 'embed-memo)
-
 (require 'shell-command)
 (shell-command-completion-mode)
 
-;; default in Emacs 23 or later
-;(require 'physical-line)
-;(setq-default physical-line-mode t)
-;(setq physical-line-ignoring-mode-list '(dired-mode))
-
-;; (require 'browse-kill-ring)
-;; (browse-kill-ring-default-keybindings)
-;; ;(global-set-key "\M-y" 'browse-kill-ring)
-;; (define-key browse-kill-ring-mode-map (kbd "j") 'browse-kill-ring-forward)
-;; (define-key browse-kill-ring-mode-map (kbd "k") 'browse-kill-ring-previous)
-;; (define-key browse-kill-ring-mode-map (kbd "C-g") 'browse-kill-ring-quit)
-;; ;(setq browse-kill-ring-display-style 'one-line)
-;; ;(setq browse-kill-ring-resize-window nil)
-
-;(require 'develock)
-;(require 'jaspace)
-
-; (require 'linum)
-; (global-linum-mode t)
-; (setq linum-format "%4d ")
+;; (require 'linum)
+;; (global-linum-mode t)
+;; (setq linum-format "%4d ")
 
 ;; (install-elisp-from-emacswiki "open-junk-file.el")
 ;; (require 'open-junk-file)
@@ -60,18 +40,9 @@
 (setq recentf-exclude '("/TAGS$" "/var/tmp"))
 (require 'recentf-ext)
 
-;; (install-elisp-from-emacswiki "tempbuf.el")
-;(require 'tempbuf)
-;(add-hook 'find-file-hooks 'turn-on-tempbuf-mode)
-;(add-hook 'dired-mode-hook 'turn-on-tempbuf-mode)
-
 ;; (install-elisp-from-emacswiki "color-moccur.el")
 (require 'color-moccur)
 (setq moccur-split-word t)
-
-;; (install-elisp-from-emacswiki "moccur-edit.el")
-;(require 'moccur-edit)
-;(setq moccur-split-word t)
 
 ;; (install-elisp-from-emacswiki "column-marker.el")
 ;(require 'column-marker)
@@ -80,32 +51,9 @@
 ;; (install-elisp "http://taiyaki.org/elisp/text-adjust/src/text-adjust.el")
 (require 'text-adjust)
 
-;; (install-elisp-from-emacswiki "summarye.el")
-;(require 'summarye)
-
-;; (install-elisp-from-emacswiki "multiverse.el")
-;(require 'multiverse)
-
-;; (install-elisp-from-emacswiki "point-undo.el")
 (require 'point-undo)
 (define-key global-map (kbd "C-z") 'point-undo)
 (define-key global-map (kbd "C-M-z") 'point-redo)
-;; (define-key global-map [f5] 'point-undo)
-;; (define-key global-map [f6] 'point-redo)
-
-;; AutoPairs
-(require 'autopair)
-(autopair-global-mode)
-(setq autopair-blink nil)
-(add-hook 'c++-mode-hook
-          #'(lambda ()
-              (push '(?< . ?>)
-                    (getf autopair-extra-pairs :code))))
-(add-hook 'python-mode-hook
-          #'(lambda ()
-              (setq autopair-handle-action-fns
-                    (list #'autopair-default-handle-action
-                          #'autopair-python-triple-quote-action))))
 
 ;; dmacro
 (defconst *dmacro-key* "\C-t" "repeat key")
--- rc.d/70ruby.el
+++ rc.d/70ruby.el
@@ -16,19 +16,10 @@
 (require 'rbenv)
 (global-rbenv-mode)
 
-;; rcodetools
+;; rcodetools and xmpfilter
 ;; gem install rcodetools
 (require 'rcodetools)
-;; xmpfilter
 (define-key ruby-mode-map (kbd "M-p") 'xmp)
-;; disable TAGS jump when 'rct-ri
-;; (setq rct-find-tag-if-available nil)
-;; ruby-mode hooks
-;; (add-hook 'ruby-mode-hook
-;;           (lambda ()
-;;             (local-set-key "\M-r" 'rct-ri)
-;;             (local-set-key "\C-cd" 'yari)
-;;             (local-set-key "\M-\C-i" 'rct-complete-symbol)))
 
 ;; ruby-dev
 ;; https://github.com/Mon-Ouie/ruby-dev.el
@@ -36,43 +27,21 @@
 (autoload 'turn-on-ruby-dev "ruby-dev" nil t)
 (add-hook 'ruby-mode-hook 'turn-on-ruby-dev)
 
-;; ruby-electric
-(require 'ruby-electric)
-(add-hook 'ruby-mode-hook '(lambda () (ruby-electric-mode t)))
-(custom-set-variables '(ruby-electric-expand-delimiters-list nil))
+(require 'ruby-end)
+(add-hook 'ruby-mode-hook
+  '(lambda ()
+    (electric-pair-mode t)
+    (electric-indent-mode t)
+    (electric-layout-mode t)))
 
 ;; ruby-block
 (require 'ruby-block)
 (ruby-block-mode t)
 (setq ruby-block-highlight-toggle t)
 
-;; snippet
-;; requires it because 'snippet-insert' is called when expanding
-;; from auto-completed word such as 'where', 'each' and so on.
-;; (require 'snippet)
-
 (require 'flymake-ruby)
 
-;; flymake for ruby
-;; Invoke ruby with '-c' to get syntax checking
-;; (require 'flymake)
-;; (defun flymake-ruby-init ()
-;;   (let* ((temp-file   (flymake-init-create-temp-buffer-copy
-;;                        'flymake-create-temp-inplace))
-;;          (local-file  (file-relative-name
-;;                        temp-file
-;;                        (file-name-directory buffer-file-name))))
-;;     (list "ruby" (list "-c" local-file))))
-;; (push '(".+\\.rb$" flymake-ruby-init) flymake-allowed-file-name-masks)
-;; (push '("Rakefile$" flymake-ruby-init) flymake-allowed-file-name-masks)
-;; (push '("^\\(.*\\):\\([0-9]+\\): \\(.*\\)$" 1 2 nil 3) flymake-err-line-patterns)
-;; (add-hook
-;;  'ruby-mode-hook
-;;  '(lambda ()
-;;     ;; Don't want flymake mode for ruby regions in rhtml files
-;;     (if (not (null buffer-file-name)) (flymake-mode))))
-
-
+;; open gem source
 ;; http://d.hatena.ne.jp/kitokitoki/20110302/p1
 (defvar helm-gem-open-ruby-command "ruby -rubygems -e 'puts Dir[\"{#{Gem::Specification.dirs.join(\",\")}}/*.gemspec\"].collect {|s| File.basename(s).gsub(/\.gemspec$/, \"\")}'")
 (defvar helm-c-source-gem-open      
