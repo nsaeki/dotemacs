aa75ab24fb9a14300dee14e7778009878d1e6416
Nobuo Saeki <nobuo.saeki@gmail.com>
2014-04-01 22:13:58 +0900

    package-refresh-contents only in first launch

--- rc.d/20package.el
+++ rc.d/20package.el
@@ -14,7 +14,8 @@
 (add-to-list 'package-archives
              '("org" . "http://orgmode.org/elpa/") t)
 
-(package-refresh-contents)
+(when (not package-archive-contents)
+  (package-refresh-contents))
 
 (defvar my/elpa-packages
   '(ac-dabbrev
