502f35a17c8685829d487e1e48255c272b4d98cf
Nobuo Saeki <nobuo.saeki@gmail.com>
2014-04-04 15:56:26 +0900

    try auto-save-buffers always on

--- rc.d/41autosave.el
+++ rc.d/41autosave.el
@@ -1,4 +1,4 @@
 (ensure-package-installed 'auto-save-buffers-enhanced)
 (require 'auto-save-buffers-enhanced)
-(auto-save-buffers-enhanced-include-only-checkout-path t)
+;; (auto-save-buffers-enhanced-include-only-checkout-path t)
 (auto-save-buffers-enhanced t)
