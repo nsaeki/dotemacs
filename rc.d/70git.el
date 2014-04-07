e600162dded1d5190c1adbb95b1dcf28b7fb524f
Nobuo Saeki <nobuo.saeki@gmail.com>
2014-04-07 21:20:01 +0900

    add helm-ls-git sources to helm-find-files

--- rc.d/70git.el
+++ rc.d/70git.el
@@ -2,9 +2,11 @@
 (require 'magit)
 ;; (key-chord-define-global "vc" 'magit-status) ;; moved to 40key-chord.el
 
-;; (ensure-package-installed 'helm-git 'helm-git-grep 'helm-git-files 'helm-ls-git)
-;; (require 'helm-git-files)
-;; (require 'helm-ls-git)
-;; (when (require 'helm-files)
-;;   (add-to-list 'helm-for-files-preferred-list helm-c-source-ls-git-status t)
-;;   (add-to-list 'helm-for-files-preferred-list helm-c-source-ls-git t))
+(ensure-package-installed 'helm-git 'helm-git-grep 'helm-git-files 'helm-ls-git)
+(require 'helm-git-files)
+(global-set-key (kbd "C-:") 'helm-git-files)
+
+(require 'helm-ls-git)
+(when (require 'helm-files)
+  (add-to-list 'helm-for-files-preferred-list helm-c-source-ls-git-status t)
+  (add-to-list 'helm-for-files-preferred-list helm-c-source-ls-git t))
