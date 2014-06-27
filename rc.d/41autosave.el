3e9b4e43996eee26c321f44d7952d9e8dc72638b
saeki <saeki@booklista.co.jp>
2014-06-27 10:43:35 +0900

    turn autosave on always

--- rc.d/41autosave.el
+++ rc.d/41autosave.el
@@ -1,5 +1,5 @@
 (ensure-package-installed 'auto-save-buffers-enhanced)
 (require 'auto-save-buffers-enhanced)
-(auto-save-buffers-enhanced-include-only-checkout-path t)
+;; (auto-save-buffers-enhanced-include-only-checkout-path t)
 (setq auto-save-buffers-enhanced-exclude-regexps '("^/scpc:" "^/ssh:" "/sudo:" "/multi:"))
 (auto-save-buffers-enhanced t)
