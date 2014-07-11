6212ae31557f0277bc4d305648184a91343d3488
Nobuo Saeki <nobuo.saeki@gmail.com>
2014-07-12 03:42:31 +0900

    git-gutter-mode

--- rc.d/70git.el
+++ rc.d/70git.el
@@ -2,11 +2,23 @@
 (require 'magit)
 ;; (key-chord-define-global "vc" 'magit-status) ;; moved to 40key-chord.el
 
-;; (ensure-package-installed 'git-gutter)
+(ensure-package-installed 'git-gutter)
 (require 'git-gutter)
+(git-gutter:linum-setup)
 ;; (ensure-package-installed 'git-gutter-fringe)
 ;; (require 'git-gutter-fringe)
+;; (setq git-gutter-fr:side 'right-fringe)
 (global-git-gutter-mode t)
+;; (custom-set-variables
+;;  '(git-gutter:modified-sign "|")
+;;  '(git-gutter:added-sign "|")
+;;  '(git-gutter:deleted-sign "|"))
+;; (set-face-background 'git-gutter:modified "plum4")
+;; (set-face-background 'git-gutter:added "SeaGreen4")
+;; (set-face-background 'git-gutter:deleted "red")
+(set-face-foreground 'git-gutter:modified "plum4")
+(set-face-foreground 'git-gutter:added "seagreen4")
+(set-face-foreground 'git-gutter:deleted "red")
 
 ;; (ensure-package-installed 'helm-git 'helm-git-grep 'helm-git-files 'helm-ls-git)
 ;; (require 'helm-git-files)
