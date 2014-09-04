65d84427325e73a7233ee1239a698eee078a4d43
Nobuo Saeki <nobuo.saeki@gmail.com>
2014-09-04 22:22:23 +0900

    add projectile cofiguration

--- rc.d/70projectile.el
+++ rc.d/70projectile.el
@@ -1,3 +1,9 @@
-(ensure-package-installed 'helm-projectile)
+(ensure-package-installed 'projectile 'helm-projectile)
+(require 'projectile)
+(projectile-global-mode)
+(setq projectile-enable-caching t)
+(define-key projectile-mode-map (kbd "M-p") 'projectile-command-map)
+
 (require 'helm-projectile)
+;; (global-set-key (kbd "C-c h") 'helm-projectile)
 (global-set-key (kbd "C-'") 'helm-projectile)
