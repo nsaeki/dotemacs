96c9a49a632076243eb44be4a684b9c3a21c2df3
Nobuo Saeki <nobuo.saeki@gmail.com>
2014-09-04 21:56:37 +0900

    revert keybinds

--- rc.d/50expand-region.el
+++ rc.d/50expand-region.el
@@ -1,4 +1,4 @@
 (ensure-package-installed 'expand-region)
 (require 'expand-region)
-(global-set-key (kbd "C-,") 'er/expand-region)
-(global-set-key (kbd "C-M-,") 'er/contract-region)
+(global-set-key (kbd "C-=") 'er/expand-region)
+(global-set-key (kbd "C-M-=") 'er/contract-region)
