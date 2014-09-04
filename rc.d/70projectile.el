5d9bb16f201ad17f23eac19f1b2b08cd38ac826b
Nobuo Saeki <nobuo.saeki@gmail.com>
2014-09-04 23:27:22 +0900

    Disable cache in projectile

--- rc.d/70projectile.el
+++ rc.d/70projectile.el
@@ -1,7 +1,6 @@
 (ensure-package-installed 'projectile 'helm-projectile)
 (require 'projectile)
 (projectile-global-mode)
-(setq projectile-enable-caching t)
 (define-key projectile-mode-map (kbd "M-p g") 'projectile-ag)
 (define-key projectile-mode-map (kbd "M-p") 'projectile-command-map)
 
