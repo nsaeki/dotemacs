158c35652454bc65aabc66f7218e6788adae8b7c
Nobuo Saeki <nobuo.saeki@gmail.com>
2014-12-20 05:50:58 +0900

    Remove default configs and unused codes.

--- init.el
+++ init.el
@@ -1,13 +1,9 @@
 (setq ring-bell-function '(lambda ()))
 (tool-bar-mode 0)
 (scroll-bar-mode 0)
-;(set-scroll-bar-mode 'right)
 (setq inhibit-startup-message t)
-(windmove-default-keybindings)
-(mouse-wheel-mode t)
-(setq mouse-wheel-follow-mouse t)
+(windmove-default-keybindings) ; Shift + Arrow Key moves window focus
 (blink-cursor-mode 0)
-(line-number-mode t)
 (column-number-mode t)
 (setq-default indent-tabs-mode nil)
 (setq-default line-spacing 0.2)
@@ -17,53 +13,36 @@
 (setq use-dialog-box nil)
 (setq read-file-name-completion-ignore-case t)
 (setq mouse-drag-copy-region t)
-;; (electric-pair-mode t) ;; use smartparens
+(global-font-lock-mode t)
+;; (electric-pair-mode t)                  ; replace smartparens
+(show-paren-mode 1)
+(setq show-paren-style 'mixed)
 (global-hl-line-mode t)
 (global-linum-mode t)
 (setq linum-format "%4d ")
-
-;; for CamelCase
-;; (global-subword-mode 1)
-
-;; do not create .# lock file
-;; (setq create-lockfiles nil)
+;; (global-subword-mode 1)                 ; for CamelCase
+(setq scroll-preserve-screen-position t)
 
 ;; scroll one line at a time (less "jumpy" than defaults)
-(setq mouse-wheel-scroll-amount '(1 ((shift) . 1))) ;; one line at a time
-(setq mouse-wheel-progressive-speed nil) ;; don't accelerate scrolling
-(setq mouse-wheel-follow-mouse 't) ;; scroll window under mouse
-(setq scroll-step 1) ;; keyboard scroll one line at a time
-
-(setq scroll-preserve-screen-position t)
+(setq mouse-wheel-scroll-amount '(1 ((shift) . 1))) ; one line at a time
+(setq mouse-wheel-progressive-speed nil) ; don't accelerate scrolling
+(setq mouse-wheel-follow-mouse 't) ; scroll window under mouse
+(setq scroll-step 1) ; keyboard scroll one line at a time
 
 ;; editing in dired-mode
 (require 'dired)
 (define-key dired-mode-map "r" 'wdired-change-to-wdired-mode)
-
-;; font-lock-mode
-(global-font-lock-mode t)
-
-;; show-paren-mode
-(show-paren-mode 1)
-(setq show-paren-style 'mixed)
-;(set-face-background 'show-paren-match-face "gray10")
-;(set-face-foreground 'show-paren-match-face "SkyBlue")
 
 ;; dabberv
 ;(setq *dabbervs-case-fold* t)
 (setq dabberv-case-fold-search nil)
 ;(setq dabbrev-case-replace nil)
 
-;; auto-mode-alist
+;; auto-mode-alist for misc file types.
 (setq auto-mode-alist
-      (append '(
-                ("Cask$" . lisp-mode)
-                ("\\.cs$" . csharp-mode)
-                ("\\.xhtml$" . sgml-mode)
-                ("\\.xsl$" . xml-mode)
+      (append '(("Cask$" . lisp-mode)
                 ("\\.xsd$" . xml-mode)
-                ("\\.rng$" . xml-mode)
-                ("\\.el$" . emacs-lisp-mode))
+                ("\\.rng$" . xml-mode))
               auto-mode-alist))
 
 ;; creates emacs backup and autosave files in backup directory
@@ -81,26 +60,10 @@
 (setq-default save-place t)
 (require 'saveplace)
 
-;; change buffer names like <2> to <dir>
-(require 'uniquify)
-(setq uniquify-buffer-name-style 'post-forward-angle-brackets)
-(setq uniquify-ignore-buffers-re "*[^*]+*")
-
-;; save bookmark
-(setq bookmark-default-file "~/.emacs.d/bookmark")
-(setq bookmark-save-flag 1)
-(progn
-  (setq bookmark-sort-flag nil)
-  (defun bookmark-arrange-latest-top ()
-    (let ((latest (bookmark-get-bookmark bookmark)))
-      (setq bookmark-alist (cons latest (delq latest bookmark-alist))))
-    (bookmark-save))
-  (add-hook 'bookmark-after-jump-hook 'bookmark-arrange-latest-top))
-
 (defun add-to-load-path-recompile (dir)
    (add-to-list 'load-path dir)
    (let (save-abbrevs) (byte-recompile-directory dir)))
-;(add-to-load-path-recompile "~/.emacs.d/site-lisp/")
+;; (add-to-load-path-recompile "~/.emacs.d/site-lisp/")
 
 (defun add-to-load-path (&rest paths)
   (let (path)
