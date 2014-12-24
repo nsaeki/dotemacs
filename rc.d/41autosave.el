35661133b69f9efc846d8ddb643b451be34bf7dc
Nobuo Saeki <saeki@booklista.co.jp>
2014-12-24 12:39:58 +0900

    Avoid new tramp buffer prefix '/scp:' from auto saving

--- rc.d/41autosave.el
+++ rc.d/41autosave.el
@@ -1,4 +1,8 @@
 ;; package: auto-save-buffers-enhanced
 ;; (auto-save-buffers-enhanced-include-only-checkout-path t)
-(setq auto-save-buffers-enhanced-exclude-regexps '("^/scpc:" "^/ssh:" "/sudo:" "/multi:"))
+(setq auto-save-buffers-enhanced-exclude-regexps '("^/scp:"
+                                                   "^/scpc:"
+                                                   "^/ssh:"
+                                                   "/sudo:"
+                                                   "/multi:"))
 (auto-save-buffers-enhanced t)
