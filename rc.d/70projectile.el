87d9cb2130706c5b01c10f815137fcd2e232f9bf
Nobuo Saeki <nobuo.saeki@gmail.com>
2014-12-26 01:41:26 +0900

    helm-projectile

--- rc.d/70projectile.el
+++ rc.d/70projectile.el
@@ -1,12 +1,5 @@
 ;; (install-packege: 'projectile 'helm-projectile)
-
-(setq projectile-keymap-prefix (kbd "C-c C-p"))
 (projectile-global-mode)
-
-(define-key projectile-mode-map (kbd "M-p g") 'projectile-ag)
 (define-key projectile-mode-map (kbd "M-p") 'projectile-command-map)
-
-;; reorder sources
-(setq helm-projectile-sources-list
-      '(helm-source-projectile-buffers-list
-        helm-source-projectile-files-list))
+(setq projectile-completion-system 'helm)
+;; (helm-projectile-toggle 1)
