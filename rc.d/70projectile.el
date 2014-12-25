a8bd1b8206f4949b87c5aa7c3281808eabd0063f
Nobuo Saeki <nobuo.saeki@gmail.com>
2014-12-26 01:46:47 +0900

    Use helm-projectile

--- rc.d/70projectile.el
+++ rc.d/70projectile.el
@@ -1,5 +1,4 @@
 ;; (install-packege: 'projectile 'helm-projectile)
 (projectile-global-mode)
 (define-key projectile-mode-map (kbd "M-p") 'projectile-command-map)
-(setq projectile-completion-system 'helm)
-;; (helm-projectile-toggle 1)
+(helm-projectile-toggle 1)
