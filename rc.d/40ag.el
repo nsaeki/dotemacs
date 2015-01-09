eb5d4dcc9dd5112bd7fe0049f13c55927da4eb00
Nobuo Saeki <nobuo.saeki@gmail.com>
2015-01-10 00:53:09 +0900

    helm-ag with symbol at point ( and remove comment in file)

--- rc.d/40ag.el
+++ rc.d/40ag.el
@@ -1,28 +1,6 @@
-;; package-installed: 'ag 'helm-ag 'wgrep-ag
-
-;; (require 'ag)
-;; (require 'helm-ag)
 (setq ag-highlight-search t)
 (setq ag-reuse-buffers t)
+(setq helm-ag-insert-at-point 'symbol)
 
-;; (require 'wgrep-ag)
 (autoload 'wgrep-ag-setup "wgrep-ag")
 (add-hook 'ag-mode-hook 'wgrep-ag-setup)
-
-;; ref. http://kotatu.org/blog/2013/12/18/emacs-ag-wgrep-for-code-grep-search/
-;; focus ag buffer after searching
-;; (defun my/filter (condp lst)
-;;   (delq nil
-;;         (mapcar (lambda (x) (and (funcall condp x) x)) lst)))
-;; (defun my/get-buffer-window-list-regexp (regexp)
-;;   "Return list of windows whose buffer name matches regexp."
-;;   (my/filter #'(lambda (window)
-;;               (string-match regexp
-;;                (buffer-name (window-buffer window))))
-;;           (window-list)))
-;; (global-set-key [(super m)]
-;;                 #'(lambda ()
-;;                     (interactive)
-;;                     (call-interactively 'ag)
-;;                     (select-window ; select ag buffer
-;;                      (car (my/get-buffer-window-list-regexp "^\\*ag ")))))
