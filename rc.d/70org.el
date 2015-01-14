bfc286628a044ec7dd97fb7f5e44d5a3dc5c6fd4
Nobuo Saeki <nobuo.saeki@gmail.com>
2015-01-14 09:35:45 +0900

    setup org-capture

--- rc.d/70org.el
+++ rc.d/70org.el
@@ -1,21 +1,25 @@
 (setq org-startup-folded 'nofold)
 (setq org-directory "~/org")
 (setq org-agenda-files (list org-directory))
+(define-key org-mode-map (kbd "C-'") nil)
 
 (setq org-default-capture-file (concat org-directory "/capture.org"))
 (setq org-capture-templates
       '(("n" "Note" entry (file org-default-capture-file)
-         "* %?\n  %U\n  %a")))
-(define-key org-mode-map (kbd "C-'") nil)
+         "* %?\n\  %i\n\n  %U\n  [[%F]]")))
 
-(defun org-insert-upheading (arg)
+(setq org-capture-templates
+      '(("n" "Note" entry (file org-default-capture-file)
+         "* %?\n  %U\n  %a")))
+
+(defun my-org-insert-upheading (arg)
   "insert upheading"
   (interactive "P")
   (org-insert-heading arg)
   (cond ((org-on-heading-p) (org-do-promote))
         ((org-at-item-p) (org-indent-item -1))))
 
-(defun org-insert-heading-dwim (arg)
+(defun my-org-insert-heading-dwim (arg)
   nil
   (interactive "p")
   (case arg
@@ -25,7 +29,8 @@
 
 (setq org-notes-directory (concat org-directory "/notes"))
 (push org-notes-directory org-agenda-files)
-(defun org-open-notes
+
+(defun my-org-open-notes
   nil
   (interactive)
   (find-file (concat org-notes-directory "/"
@@ -34,14 +39,25 @@
   (goto-char (point-max))
   (org-show-entry))
 
-(defun org-open-notes-with-name
+(defun my-org-open-notes-with-name
   nil
   (interactive)
-  (let ((notename (read-string "Open org file with name: ")))
-    (find-file (concat org-notes-directory "/"
-                       (format-time-string "%Y%m%d")
-                       (when (> (length notename) 0)
-                         (concat "-" notename))
-                       ".org")))
-  (goto-char (point-max))
-  (org-show-entry))
+  (let ((notename (read-string "Open org file with name: "
+                               (concat (format-time-string "%Y%m%d")
+                                       "-"))))
+    (find-file (concat org-notes-directory "/" notename ".org"))
+    (goto-char (point-max))
+    (org-show-entry)))
+
+(defun my-org-quick-note
+    nil
+  (interactive)
+  (let* ((current-org-capture-file
+          (concat org-notes-directory "/"
+                  (format-time-string "%Y%m%d")
+                  ".org"))
+         (org-capture-templates
+          '(("c" "Capture" entry (file+headline current-org-capture-file "Capture")
+             "* %?\n\n  %i\n\n  %U\n  [[%F]]")))
+         (org-capture-entry (org-capture-select-template  "c")))
+    (org-capture)))
--- rc.d/90keybinds.el
+++ rc.d/90keybinds.el
@@ -41,8 +41,8 @@
 (global-set-key (kbd "C-c c") 'smart-compile)
 (global-set-key (kbd "C-c d") 'dash-at-point)
 (global-set-key (kbd "C-c e") 'dash-at-point-with-docset)
-(global-set-key (kbd "C-c n") 'org-open-notes)
-(global-set-key (kbd "C-c m") 'org-capture)
+(global-set-key (kbd "C-c n") 'my-org-open-notes)
+(global-set-key (kbd "C-c m") 'my-org-quick-note)
 (global-set-key (kbd "C-c l") 'toggle-truncate-lines)
 
 ;; smartprep
