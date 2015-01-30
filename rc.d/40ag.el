bd736c11d9464efbed7d2f4d96c794e1543cba6b
Nobuo Saeki <nobuo.saeki@gmail.com>
2015-01-31 01:02:56 +0900

    Add advice to helm-ag

--- rc.d/40ag.el
+++ rc.d/40ag.el
@@ -1,6 +1,20 @@
 (setq ag-highlight-search t)
 (setq ag-reuse-buffers t)
+
+;; helm-ag
 (setq helm-ag-insert-at-point 'symbol)
+
+(defun my-helm-ag--query ()
+  (let* ((searched-word (helm-ag--searched-word))
+         (prompt
+          (format "Search string%s: "
+                  (if (and searched-word
+                           (not (string-empty-p searched-word)))
+                      (format " (default %s)" searched-word)
+                    "")))
+         (query (read-string prompt nil 'helm-ag--command-history)))
+    (setq helm-ag--last-query query)))
+(advice-add 'helm-ag--query :override 'my-helm-ag--query)
 
 (autoload 'wgrep-ag-setup "wgrep-ag")
 (add-hook 'ag-mode-hook 'wgrep-ag-setup)
