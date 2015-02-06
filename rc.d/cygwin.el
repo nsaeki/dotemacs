30113384b780365a8da0bcdda7e88673aadac80d
Nobuo Saeki <nobuo.saeki@gmail.com>
2015-02-07 00:36:15 +0900

    Check system in each systems el

--- rc.d/cygwin.el
+++ rc.d/cygwin.el
@@ -1,25 +1,25 @@
-;; PATH
-(setq exec-path (cons "c:/cygwin/bin" exec-path))
-(setenv "PATH" (concat '"c:/cygwin/bin:" (getenv "PATH")))
-;(setq exec-path (cons "c:/cygwin/usr/bin" exec-path))
-;(setenv "PATH" (concat '"c:/cygwin/usr/bin:" (getenv "PATH")))
-;(setq exec-path (cons "c:/cygwin/usr/local/bin" exec-path))
-;(setenv "PATH" (concat '"c:/cygwin/usr/local/bin:" (getenv "PATH")))
+(when (eq system-type 'windows-nt)
+  ;; PATH
+  (setq exec-path (cons "c:/cygwin/bin" exec-path))
+  (setenv "PATH" (concat '"c:/cygwin/bin:" (getenv "PATH")))
+  ;; (setq exec-path (cons "c:/cygwin/usr/bin" exec-path))
+  ;; (setenv "PATH" (concat '"c:/cygwin/usr/bin:" (getenv "PATH")))
+  ;; (setq exec-path (cons "c:/cygwin/usr/local/bin" exec-path))
+  ;; (setenv "PATH" (concat '"c:/cygwin/usr/local/bin:" (getenv "PATH")))
 
-;; cygwin
-(require 'cygwin-mount)
+  ;; shell command
+  (add-hook 'comint-output-filter-functions
+            'shell-strip-ctrl-m nil t)
+  (add-hook 'comint-output-filter-functions
+            'comint-watch-for-password-prompt nil t)
+  (setq explicit-shell-file-name "bash.exe")
+  ;; For subprocesses invoked via the shell
+  ;; (e.g., "shell -c command")
+  (setq shell-file-name explicit-shell-file-name)
 
-;; shell command
-(add-hook 'comint-output-filter-functions
-          'shell-strip-ctrl-m nil t)
-(add-hook 'comint-output-filter-functions
-          'comint-watch-for-password-prompt nil t)
-(setq explicit-shell-file-name "bash.exe")
-;; For subprocesses invoked via the shell
-;; (e.g., "shell -c command")
-(setq shell-file-name explicit-shell-file-name)
+  ;; Locale
+  (setenv "LANG" "C")
 
-;; Locale
-(setenv "LANG" "C")
-
-(cygwin-mount-activate)
+  ;; cygwin
+  (require 'cygwin-mount)
+  (cygwin-mount-activate))
