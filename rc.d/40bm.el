44ee2228547e718aff83cb51a5eb937b62e1909a
Nobuo Saeki <nobuo.saeki@gmail.com>
2015-02-06 23:22:47 +0900

    Restore bm settings

--- rc.d/40bm.el
+++ rc.d/40bm.el
@@ -1,34 +1,20 @@
 ;; copied from bm.el
-;; Make sure the repository is loaded as early as possible
-(setq bm-restore-repository-on-load t)
-(require 'bm)
-
-;; Loading the repository from file when on start up.
-(add-hook 'after-init-hook 'bm-repository-load)
-
-;; Restoring bookmarks when on file find.
-(add-hook 'find-file-hooks 'bm-buffer-restore)
-
-;; Saving bookmark data on killing a buffer
-(add-hook 'kill-buffer-hook 'bm-buffer-save)
-
-;; Saving the repository to file when on exit.
-;; kill-buffer-hook is not called when Emacs is killed, so we
-;; must save all bookmarks first.
-;; (add-hook 'kill-emacs-hook '(lambda nil
-;;                               (bm-buffer-save-all)
-;;                               (bm-repository-save)))
-
-;; Update bookmark repository when saving the file.
-(add-hook 'after-save-hook 'bm-buffer-save)
-
-;; Restore bookmarks when buffer is reverted.
-(add-hook 'after-revert-hook 'bm-buffer-restore)
-
-(setq-default bm-buffer-persistence t)
 (setq bm-repository-file "~/.emacs.d/bm-repository")
+(setq bm-restore-repository-on-load t)
+(setq-default bm-buffer-persistence t)
+
+(add-hook 'after-init-hook 'bm-repository-load)
+(add-hook 'find-file-hooks 'bm-buffer-restore)
+(add-hook 'after-revert-hook 'bm-buffer-restore)
+(add-hook 'after-save-hook 'bm-buffer-save)
 (add-hook 'vc-before-checkin-hook 'bm-buffer-save)
+(add-hook 'kill-buffer-hook 'bm-buffer-save)
+(add-hook 'kill-emacs-hook '(lambda nil
+                              (bm-buffer-save-all)
+                              (bm-repository-save)))
 
 (setq bm-persistent-face 'bm-face)
 (custom-set-faces
-  '(bm-face ((t (:background "#595900")))))
+ '(bm-face ((t (:background "#595900")))))
+
+(require 'bm)
