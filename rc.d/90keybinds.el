0e4e657147f7b069aa040411369b84d14ab8d97e
Nobuo Saeki <nobuo.saeki@gmail.com>
2015-02-06 17:12:16 +0900

    C-,

--- rc.d/90keybinds.el
+++ rc.d/90keybinds.el
@@ -27,8 +27,8 @@
 
 (global-set-key (kbd "C-;") 'helm-mini)
 (global-set-key (kbd "C-'") 'helm-ls-git-ls)
+(global-set-key (kbd "C-,") 'er/expand-region)
 (global-set-key (kbd "C-.") 'helm-imenu)
-(global-set-key (kbd "C-,") 'helm-bm)
 (global-set-key (kbd "M-x") 'helm-M-x)
 (global-set-key (kbd "M-y") 'helm-show-kill-ring)
 (global-set-key (kbd "M-o") 'helm-occur)
