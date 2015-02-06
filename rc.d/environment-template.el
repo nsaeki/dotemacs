e9da47af8eb03195861cf2fbf57c21c36586172f
Nobuo Saeki <nobuo.saeki@gmail.com>
2015-02-07 00:35:19 +0900

    Clean up

--- rc.d/environment-template.el
+++ rc.d/environment-template.el
@@ -1,22 +1,16 @@
-;; frame size
+;; Initial Frame size
 (setq initial-frame-alist
       (append '((top . 32)
                 (left . 630)
                 (width . 110)
                 (height . 60))
-	      initial-frame-alist))
+              initial-frame-alist))
 (setq default-frame-alist initial-frame-alist)
-;(split-window-horizontally)
+(split-window-horizontally)
 
-;(when (eq window-system 'mac)
-;  (add-hook 'window-setup-hook
-;            (lambda ()
-;              (set-frame-parameter nil 'fullscreen 'fullboth)
-;              )))
-
-;(if window-system (progn
-;   (set-frame-parameter nil 'alpha 100)
-;   ))
+;; Transparency
+(if window-system
+    (progn (set-frame-parameter nil 'alpha 100)))
 
 ;; http proxy
-;; (setq url-proxy-services '(("http" . "proxy.host:8080")))
+(setq url-proxy-services '(("http" . "proxy.host:8080")))
