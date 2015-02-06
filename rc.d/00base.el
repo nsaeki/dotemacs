185e5aa59728eec83c04dc22c19c8abb106f9fb2
Nobuo Saeki <nobuo.saeki@gmail.com>
2015-02-07 00:19:39 +0900

    Add some comments and reorder

--- rc.d/00base.el
+++ rc.d/00base.el
@@ -1,33 +1,36 @@
 (setq ring-bell-function '(lambda ()))
-(tool-bar-mode 0)
-(scroll-bar-mode 0)
 (setq inhibit-startup-message t)
-(windmove-default-keybindings) ; Shift + Arrow Key moves window focus
-(blink-cursor-mode 0)
-(column-number-mode t)
-(setq-default indent-tabs-mode nil)
-(setq-default line-spacing 0.2)
-(savehist-mode 1)
-(ffap-bindings)
-(defalias 'yes-or-no-p 'y-or-n-p)
-(setq use-dialog-box nil)
 (setq completion-ignore-case t)
 (setq mouse-drag-copy-region t)
-(global-font-lock-mode t)
-;; (electric-pair-mode t)                  ; replaced by smartparens
-(show-paren-mode 1)
-(setq show-paren-style 'mixed)
-(global-hl-line-mode t)
-(global-linum-mode t)
-(setq linum-format "%4d ")
-;; (global-subword-mode 1)                 ; for CamelCase
 (setq scroll-preserve-screen-position t)
 (setq eval-expression-print-length nil)
 (setq eval-expression-print-level nil)
-(which-function-mode t)
-(global-auto-revert-mode 1)
 
-(setq-default mode-line-format          ; almost all are default
+(ffap-bindings)
+(tool-bar-mode 0)
+(scroll-bar-mode 0)
+(blink-cursor-mode 0)
+(show-paren-mode 1)
+(setq show-paren-style 'mixed)
+(savehist-mode 1)
+(setq linum-format "%4d ")
+(global-linum-mode t)
+(column-number-mode t)
+(global-font-lock-mode t)
+(global-hl-line-mode t)
+;; (electric-pair-mode t)                  ; use smartparens
+(global-auto-revert-mode 1)
+(which-function-mode t)
+;; (global-subword-mode 1)                 ; for CamelCase
+(setq-default indent-tabs-mode nil)
+(setq-default line-spacing 0.2)         ; a little wider
+(defalias 'yes-or-no-p 'y-or-n-p)
+(setq use-dialog-box nil)
+
+;; Shift + Arrow Key moves window focus
+(windmove-default-keybindings)
+
+(setq-default mode-line-format
               '("%e" mode-line-front-space
                 mode-line-mule-info
                 mode-line-client
@@ -40,24 +43,30 @@
                 smartrep-mode-line-string
                 (vc-mode vc-mode)
                 "  "
-                mode-line-misc-info ; prepend this to mode-line-modes to display which-function-mode
+                ;; Make visible which-function-mode info
+                mode-line-misc-info
                 mode-line-modes
                 mode-line-end-spaces))
 
-;; scroll one line at a time (less "jumpy" than defaults)
+;; Scroll one line at a time (less "jumpy" than defaults)
 (setq mouse-wheel-scroll-amount '(1 ((shift) . 1))) ; one line at a time
 (setq mouse-wheel-progressive-speed nil) ; don't accelerate scrolling
 (setq mouse-wheel-follow-mouse 't) ; scroll window under mouse
 (setq scroll-step 1) ; keyboard scroll one line at a time
 
-;; editing in dired-mode
-(require 'dired)
-(define-key dired-mode-map "r" 'wdired-change-to-wdired-mode)
-
 ;; dabberv
 ;(setq *dabbervs-case-fold* t)
 (setq dabberv-case-fold-search nil)
 ;(setq dabbrev-case-replace nil)
+
+;; Remembers cursor position
+(setq save-place-file "~/.emacs.d/save-places")
+(setq-default save-place t)
+(require 'saveplace)
+
+;; Editing in dired-mode
+(require 'dired)
+(define-key dired-mode-map "r" 'wdired-change-to-wdired-mode)
 
 ;; auto-mode-alist for misc file types.
 (setq auto-mode-alist
@@ -66,7 +75,7 @@
                 ("\\.rng$" . xml-mode))
               auto-mode-alist))
 
-;; creates emacs backup and autosave files in backup directory
+;; Creates backup and autosave files in backup directory
 (defvar user-temporary-file-directory "~/.emacs.d/backup")
 (make-directory user-temporary-file-directory t)
 (setq make-backup-files nil)
@@ -75,8 +84,3 @@
 (setq auto-save-default nil)
 (setq auto-save-file-name-transforms
       `((".*" ,user-temporary-file-directory t)))
-
-;; remembers cursor position
-(setq save-place-file "~/.emacs.d/save-places")
-(setq-default save-place t)
-(require 'saveplace)
