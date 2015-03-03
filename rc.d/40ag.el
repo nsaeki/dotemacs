28c7d40cbba4b9d738e287cd1da56145632e849e
Nobuo Saeki <nobuo.saeki@gmail.com>
2015-03-03 19:06:16 +0900

    helm-ag starts empty input

--- rc.d/40ag.el
+++ rc.d/40ag.el
@@ -1,6 +1,6 @@
 (setq ag-highlight-search t)
 (setq ag-reuse-buffers t)
-(setq helm-ag-insert-at-point 'symbol)
+;; (setq helm-ag-insert-at-point 'symbol)
 
 (autoload 'wgrep-ag-setup "wgrep-ag")
 (add-hook 'ag-mode-hook 'wgrep-ag-setup)
