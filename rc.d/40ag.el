a79589300d39069f46e60bf14ebe4318603d3ba6
Nobuo Saeki <nobuo.saeki@gmail.com>
2015-05-26 01:26:41 +0900

    Fix wgrep-ag keybinding

--- rc.d/40ag.el
+++ rc.d/40ag.el
@@ -2,6 +2,7 @@
 (setq ag-reuse-buffers t)
 ;; (setq helm-ag-insert-at-point 'symbol)
 
-(autoload 'wgrep-ag-setup "wgrep-ag")
 (add-hook 'ag-mode-hook 'wgrep-ag-setup)
-(define-key ag-mode-map (kbd "r") 'wgrep-change-to-wgrep-mode)
+(eval-after-load 'ag
+  '(progn
+     (define-key ag-mode-map (kbd "r") 'wgrep-change-to-wgrep-mode)))
