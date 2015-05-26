88dd14149d5f3a31db360a0b7bd624032c86bd10
Nobuo Saeki <nobuo.saeki@gmail.com>
2015-05-27 00:49:32 +0900

    my/org-journal functions

--- rc.d/70org.el
+++ rc.d/70org.el
@@ -1,7 +1,7 @@
 (setq org-startup-folded 'nofold)
-(setq org-directory "~/org")
+(setq org-directory "~/notes")
 (setq org-agenda-files (list org-directory))
-(setq org-default-notes-file (concat org-directory "/notes.org"))
+(setq org-default-notes-file (concat org-directory "/scribble.org"))
 
 (define-key org-mode-map (kbd "C-'") nil)
 (define-key org-mode-map (kbd "C-c a") 'org-agenda)
@@ -21,8 +21,8 @@
     (16 (org-insert-upheading nil))     ; C-u C-u
     (t  (org-insert-heading nil))))
 
-(setq my-org-notes-directory (concat org-directory "/journal"))
-(push my-org-notes-directory org-agenda-files)
+(setq my/org-journal-directory (concat org-directory "/journal"))
+(push my/org-journal-directory org-agenda-files)
 
 (setq org-capture-templates
       '(("n" "Add new item to default note file." entry (file nil)
@@ -30,31 +30,20 @@
          :empty-lines 1)
         ))
 
-(defun my-org-current-note ()
-  "Returns today's note file path as YYYYMMDD.org"
-  (concat my-org-notes-directory "/" (format-time-string "%Y%m%d") ".org"))
-
-(defun my-org-open-note ()
-  "Open today's notes file"
-  ;; TODO: With integer arguments, open previous file in notes dir.
-  ;; 0 means today (default) and -1 means previous file.
-  ;; C-u has same effect.
+(defun my/org-open-journal (&optional filename)
   (interactive)
-  (find-file (my-org-current-note))
-  (goto-char (point-max))
-  (org-show-entry))
-
-(defun my-org-open-note-with-name ()
-  nil
-  (interactive)
-  (let ((notename (read-string "Open note file with name: "
-                               (concat (format-time-string "%Y%m%d")
-                                       "-"))))
-    (find-file (concat my-org-notes-directory "/" notename ".org"))
+  (let ((filename (or filename (format-time-string "%Y%m%d"))))
+    (find-file (concat my/org-journal-directory "/" filename ".org"))
     (goto-char (point-max))
     (org-show-entry)))
 
-(defun my-org-quick-capture ()
-  nil
+(defun my/org-open-journal-with-title (&optional title)
+  (interactive)
+  (let ((filename (if title (concat (format-time-string "%Y%m%d-") title)
+                (read-string "Open journal file with name: "
+                             (format-time-string "%Y%m%d-")))))
+    (my/org-open-journal filename)))
+
+(defun my/org-quick-capture ()
   (interactive)
   (org-capture nil "n"))
