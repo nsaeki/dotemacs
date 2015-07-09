8373e634aea5c835c511af534cae961fe363caa0
Nobuo Saeki <nobuo.saeki@gmail.com>
2015-07-09 19:41:33 +0900

    Disable popwin for helm buffer

--- rc.d/40popwin.el
+++ rc.d/40popwin.el
@@ -3,4 +3,4 @@
 (setq popwin:popup-window-height 20)
 
 ;; for helm
-(push '("^\*helm.*" :regexp 1 :height 25) popwin:special-display-config)
+;; (push '("^\*helm.*" :regexp 1 :height 25) popwin:special-display-config)
