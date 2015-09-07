828e57d1fc72d265b50314c0037b9926bec85a12
Nobuo Saeki <nobuo.saeki@gmail.com>
2015-09-07 23:07:03 +0900

    iPython

--- rc.d/disabled/70python.el
+++ rc.d/disabled/70python.el
@@ -4,41 +4,9 @@
  python-shell-interpreter-args ""
  python-shell-prompt-regexp "In \\[[0-9]+\\]: "
  python-shell-prompt-output-regexp "Out\\[[0-9]+\\]: "
- python-shell-completion-setup-code ""
+ python-shell-completion-setup-code
+   "from IPython.core.completerlib import module_completion"
+ python-shell-completion-module-string-code
+   "';'.join(module_completion('''%s'''))\n"
  python-shell-completion-string-code
- "';'.join(__IP.complete('''%s'''))\n")
-
-;(autoload 'python-mode "python-mode" "Python editing mode." t)
-;(add-to-list 'auto-mode-alist '("\\.py\\'" . python-mode))
-;(add-to-list 'interpreter-mode-alist '("python" . python-mode))
-
-;; flymake for python
-;; http://tech.lampetty.net/tech/index.php/archives/380
-;; (require 'flymake)
-;; (defun flymake-python-init ()
-;;   (let* ((temp-file (flymake-init-create-temp-buffer-copy
-;;                      'flymake-create-temp-inplace))
-;;          (local-file (file-relative-name
-;;                       temp-file
-;;                       (file-name-directory buffer-file-name))))
-;;     (list "pyflakes" (list local-file))))
- 
-;; (defconst flymake-allowed-python-file-name-masks
-;;   '(("\\.py$" flymake-python-init)))
-;; (defvar flymake-python-err-line-patterns
-;;   '(("\\(.*\\):\\([0-9]+\\):\\(.*\\)" 1 2 nil 3)))
- 
-;; (defun flymake-python-load ()
-;;   (interactive)
-;;   (defadvice flymake-post-syntax-check
-;;     (before flymake-force-check-was-interrupted)
-;;     (setq flymake-check-was-interrupted t))
-;;   (ad-activate 'flymake-post-syntax-check)
-;;   (setq flymake-allowed-file-name-masks
-;;         (append flymake-allowed-file-name-masks
-;;                 flymake-allowed-python-file-name-masks))
-;;   (setq flymake-err-line-patterns
-;;         (append flymake-err-line-patterns
-;;                 flymake-python-err-line-patterns))
-;;   (flymake-mode t))
-;; (add-hook 'python-mode-hook '(lambda () (flymake-python-load)))
+   "';'.join(get_ipython().Completer.all_completions('''%s'''))\n")
