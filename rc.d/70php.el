df784c6260bcc039d612963b0d7e55c4b05988c7
Nobuo Saeki <nobuo.saeki@gmail.com>
2016-10-07 01:58:27 +0900

    Include php-imenu

--- rc.d/70php.el
+++ rc.d/70php.el
@@ -2,3 +2,14 @@
           '(lambda ()
              (local-set-key (kbd "C-.") 'helm-imenu)
              (local-set-key (kbd "C-c C-.") 'php-show-arglist)))
+
+;; php-imenu
+(autoload 'php-imenu-create-index "php-imenu" nil t)
+;; Add the index creation function to the php-mode-hook
+;; In php-mode 1.2, it's php-mode-user-hook.  In 1.4, it's php-mode-hook.
+(add-hook 'php-mode-hook 'php-imenu-setup)
+(defun php-imenu-setup ()
+  (setq imenu-create-index-function (function php-imenu-create-index))
+  ;; uncomment if you prefer speedbar:
+  ;(setq php-imenu-alist-postprocessor (function reverse))
+  (imenu-add-menubar-index))
