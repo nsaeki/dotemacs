3f44e75ff52021e58b25cd0916ec0525f7093200
Nobuo Saeki <nobuo.saeki@gmail.com>
2019-02-05 22:50:07 +0900

    Add melpa-stble

--- init.el
+++ init.el
@@ -1,5 +1,6 @@
 (require 'package)
 (add-to-list 'package-archives '("melpa" . "http://melpa.milkbox.net/packages/") t)
+(add-to-list 'package-archives '("melpa-stable" . "https://stable.melpa.org/packages/") t)
 (add-to-list 'package-archives '("org" . "http://orgmode.org/elpa/") t)
 (package-initialize)
 
